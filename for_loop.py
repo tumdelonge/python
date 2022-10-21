1cd284775ef8d55cd3764cafa63700a89823d2e7
cahyo panitis s <84408523+tumdelonge@users.noreply.github.com>
2022-10-21 22:52:46 +0700

    looping_python
    python looping

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+# looping
+# while loop
+# for loop
+# nested loop
+
+# 2. for_loop
+# for i in range(10):
+#     print("cetak ke-",i)
+
+# # coba2
+# for i in range(1, 10):
+#     print("cetak ke-",i)
+
+# # coba3
+# for i in range(2, 11, 2):
+#     print("angka ke-",i)
+
+
+# # coba4
+# for i in range(1, 11, 2):
+#     print("angka ke-",i)
+
+# coba5
+listBilangan = [1,2,3,4,5,6,7,8,9,1,22,23,46,78,90,100]
+for bilangan in listBilangan:
+    if bilangan % 2 == 1:
+        print("angka", bilangan,"bilangan ganjil")
---
+++ nested_loop.py
@@ -0,0 +1,27 @@
+# looping
+# while loop
+# for loop
+# nested loop
+
+# # 3. nested_loop
+# # for di dalam for
+# listcofee = ['americano', 'cappucino','latte', 'espresso']
+# listType = ['ice', 'hot']
+# for type in listType:
+#     for cofee in listcofee:
+#         print(type, " ", cofee)
+
+# 
+
+# # membangkitkan bilangan random
+# import random
+# listData = ['zonk', '100.000', '5000', 'Jackpot 999.999.999', 'zonk']
+# x = random.random()
+# if x <= 0.33:
+#     print("zonk")
+# elif x <= 0.66:
+#     print("5000")
+# elif x < 0.98:
+#     print("1000")
+# else:
+#     print("JackPOT")
